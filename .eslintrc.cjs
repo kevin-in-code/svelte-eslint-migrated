b3259036dab4e48bf21ae2cffc2347df6eb4f36a
Kevin A. Naudé <1943178+kevin-a-naude@users.noreply.github.com>
2022-10-11 22:34:13 +1100

    Migrated from eslint-plugin-svelte3 to eslint-plugin-svelte, svelte-eslint-parser

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,29 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended'
+	],
+	plugins: ['svelte', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: 'tsconfig.eslint.json',
+		extraFileExtensions: ['.svelte']
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	env: {
 		browser: true,
 		es2017: true,
